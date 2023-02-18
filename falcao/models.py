8512950d41e15ab75a6932408b3af1bd1c813eb4
unknown <carloseduardoguedes89981456761@gmail.com>
2023-02-18 10:24:36 -0300

    second commit

--- falcao/models.py
+++ falcao/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
-# Create your models here.
+class produtos(models.Model):
+    nome=models.CharField(max_length=255)
+    valor=models.FloatField()
+    quantidade=models.IntegerField()
+    imagem=models.ImageField()
+    descricao=models.TextField()
+    def __str__(self):
+        return self.nome
+class vendas(models.Model):
+    produtos=models.ForeignKey(produtos,on_delete=models.CASCADE)
+    quantidade=models.IntegerField()
+    data=models.DateField()
+    def __str__(self):
+        return self.produtos.nome
